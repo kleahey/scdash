68ba70290456597e6223ec0a58532f4430cbe8d8
Kevin Leahey <kleahey@me.com>
2016-12-23 09:08:46 -0500

    Hipchat API post format

---
+++ jobs/hipchat.rb
@@ -0,0 +1,10 @@
+HTTParty.post("https://api.hipchat.com/v2/room/421939/notification",
+  :query => {
+    :color => "yellow",
+    :message => "<strong>Please check the Briefing Board for updates!</strong>",
+    :notify => "true",
+    :message_format => "html"
+  },
+  :headers => {
+    :Authorization => "Bearer im2tvoAJxRBa9Z8XP9FK6Ke82x1esTora650bhEu"
+  })
