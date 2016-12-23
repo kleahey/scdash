43cd356f0caa7e4b53c6ea95a5f6ab7179137657
Kevin Leahey <kleahey@me.com>
2016-12-22 20:45:48 -0500

    working release 1

--- jobs/tickets.rb
+++ jobs/tickets.rb
@@ -3,7 +3,7 @@ require 'mysql2'
 SCHEDULER.every '5m', :first_in => 0 do |job|
 
   # Myql connection
-  db = Mysql2::Client.new(:host => "cauniversity.net", :username => "root", :password => "Planet@ry7", :port => 3306, :database => "SATA" )
+  db = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "Planet@ry7", :port => 3306, :database => "SATA" )
 
   # Mysql query
   sql = "SELECT csr, (SUM(app)+SUM(rec)+SUM(mem)) AS totals FROM tickets WHERE date=(SELECT MAX(date) FROM tickets) GROUP BY csr HAVING (SUM(app)+SUM(rec)+SUM(mem)) > 0 ORDER BY totals DESC"
--- jobs/welcome.rb
+++ jobs/welcome.rb
@@ -1,12 +1,12 @@
 require 'mysql2'
 
-SCHEDULER.every '10s', :first_in => 0 do |job|
+SCHEDULER.every '2m', :first_in => 0 do |job|
 
   # Myql connection
-  db = Mysql2::Client.new(:host => "cauniversity.net", :username => "root", :password => "Planet@ry7", :port => 3306, :database => "moodledb" )
+  db = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "Planet@ry7", :port => 3306, :database => "moodledb" )
 
   # Mysql query
-  sql = "SELECT content FROM mdl_data_content WHERE fieldid = 64 AND recordid=(SELECT MAX(recordid) FROM mdl_data_content)"
+  sql = "SELECT content FROM mdl_data_content WHERE fieldid = 64 AND recordid=(SELECT MAX(recordid) FROM mdl_data_content WHERE fieldid = 64)"
 
   # Execute the query
   results = db.query(sql)
