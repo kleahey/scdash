a23d6e899429a903c30209347b4525e66cbe5cd0
Kevin Leahey <kleahey@me.com>
2016-12-23 09:41:03 -0500

    Small update to code

--- jobs/todaysCalls.rb
+++ jobs/todaysCalls.rb
@@ -27,17 +27,6 @@ ticketTotals = data.map do |row|
     }
   end
 
-  HTTParty.post("https://api.hipchat.com/v2/room/421939/notification",
-    :query => {
-      :color => "yellow",
-      :message => "<strong>Please check the Briefing Board for updates!</strong>",
-      :notify => "true",
-      :message_format => "html"
-    },
-    :headers => {
-      :Authorization => "Bearer im2tvoAJxRBa9Z8XP9FK6Ke82x1esTora650bhEu"
-    })
-
 send_event('todaysCalls', { items: ticketTotals })
 
 end
