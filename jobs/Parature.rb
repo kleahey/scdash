629de6fee1330d21084396d871851496ac9e395e
Kevin Leahey <kleahey@me.com>
2017-10-03 15:55:37 -0400

    added fix to chat info in Parature

--- jobs/Parature.rb
+++ jobs/Parature.rb
@@ -121,29 +121,35 @@ ticketTotals = counts.map do |k, v|
 end
 
 # List all chat totals by Team Member
-chat_array = []
+chatArray = []
 
-applicant_chat = applicant_requests($configuration, "Chat?_total_=false&Date_Created_min_=#{Time.now.strftime('%Y-%m-%d')}T05:00:00Z&Date_Ended_min_=_today_", :get, nil)
+applicantChat = applicant_requests($configuration, "Chat?_total_=false&Date_Ended=#{Time.now.strftime('%Y-%m-%d')}&_pageSize_=200", :get, nil)
 
 begin
-  applicant_chat['Chat'].map { |x| chat_array.push(x["Initial_Csr"][0]["Csr"][0]["Full_Name"][0]["content"]) }
+  applicantChat["Chat"].map do |x|
+    next if x["Initial_Csr"].nil?
+    chatArray.push(x["Initial_Csr"][0]["Csr"][0]["Full_Name"][0]["content"])
+  end
 rescue
   puts "Error reading Applicant chats."
 end
 
-recommender_chat = recommender_requests($configuration, "Chat?_total_=false&Date_Created_min_=#{Time.now.strftime('%Y-%m-%d')}T05:00:00Z&Date_Ended_min_=_today_", :get, nil)
+recommenderChat = recommender_requests($configuration, "Chat?_total_=false&Date_Ended=#{Time.now.strftime('%Y-%m-%d')}&_pageSize_=200", :get, nil)
 
 begin
-  recommender_chat['Chat'].each { |x| chat_array.push(x["Initial_Csr"][0]["Csr"][0]["Full_Name"][0]["content"]) }
+  recommenderChat["Chat"].map do |x|
+    next if x["Initial_Csr"].nil?
+    chatArray.push(x["Initial_Csr"][0]["Csr"][0]["Full_Name"][0]["content"])
+  end
 rescue
   puts "Error reading Recommender chats."
 end
 
-chat_counts = Hash.new(0)
-chat_array.each { |array| chat_counts[array] += 1 }
+chatCounts = Hash.new(0)
+chatArray.each { |array| chatCounts[array] += 1 }
 
-chat_counts = chat_counts.sort_by { |k, v| v }.reverse
-chatTotals = chat_counts.map { |k, v| row = { :label => k, :value => v } }
+chatCounts = chatCounts.sort_by { |k, v| v }.reverse
+chatTotals = chatCounts.map { |k, v| row = { :label => k, :value => v } }
 
 #Send job information to widgets
 send_event('activeAppTickets', { value: activeAppTickets } )
