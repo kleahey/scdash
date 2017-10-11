c74a8db9435fc93909fbe9216bee028f13479936
Kevin Leahey <kleahey@me.com>
2017-10-10 21:13:34 -0400

    hipchat added

--- jobs/hipchat.rb
+++ jobs/hipchat.rb
@@ -8,3 +8,27 @@
 #   :headers => {
 #     :Authorization => "Bearer im2tvoAJxRBa9Z8XP9FK6Ke82x1esTora650bhEu"
 #   })
+
+require "hipchat"
+require "json"
+require "time"
+
+SCHEDULER.every '1m', :first_in => 0 do |job|
+
+  client = HipChat::Client.new("qPfNioZgUtyMxsqOhtvyATwqRBfxUmoc5ikj4fwz")
+
+  result = client['DevDash'].history()
+
+  result = JSON.parse(result)
+
+  message = []
+
+  result["items"].map do |x|
+    message.push(x["message"])
+  end
+
+  welcome = message.last
+
+  send_event('dailyBriefing', {text: welcome})
+
+end
